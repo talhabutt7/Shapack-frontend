59ae692245e4bc78bccb4de11582c9ad131ac75a
talhabutt7 <talhabuttt@gmail.com>
2024-06-24 17:48:38 +0100

    Wrap the App component with GlobalProvider

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GlobalProvider } from './context/GlobalState';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+  <GlobalProvider>
     <App />
-  </React.StrictMode>
+  </GlobalProvider>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
