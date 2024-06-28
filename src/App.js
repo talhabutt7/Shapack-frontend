9f854f83e0910b445de59552b82e2ca0fbe7a529
talhabutt7 <talhabuttt@gmail.com>
2024-06-28 18:08:57 +0100

    Importing Login, Registration and AuthContext into App.js

--- src/App.js
+++ src/App.js
@@ -3,18 +3,25 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Products from './components/Products';
 import Cart from './components/Cart';
+import Login from './components/Login';
+import Register from './components/Register';
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
-      </div>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
